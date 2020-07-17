821b404e42c3565439423a377db8b81672f5171c
unknown <keng.thao@hotmail.com>
2020-07-16 19:52:01 -0500

    met MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react"
-import useLocalStorage from "./useLocalStorage"
+import { useEffect } from "react"
+import { useLocalStorage } from "./useLocalStorage"
 
 export const useDarkMode = (initialValue) => {
     const [darkMode, setDarkMode] = useLocalStorage(initialValue);
